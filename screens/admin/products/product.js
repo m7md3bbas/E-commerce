a069f4b58a7b0099f5a5c499a4e571699cdd2f7e
Mohammad-Eldabaa <maldb0907@gmail.com>
2025-05-11 18:02:27 +0300

    enhance delete

--- screens/admin/products/product.js
+++ screens/admin/products/product.js
@@ -92,13 +92,38 @@ $(".form-select").on("change", function (e) {
 showItem();
 
 $(document).on("click", ".deletePro", function () {
-  let result = confirm("Are you sure?");
-
-  if (result) {
-    let productId = $(this).data("id");
-    // console.log(userId);
-    deleteProduct(productId);
-    alert("Product deleted successfully");
-    location.reload();
-  }
+  checkDelete().then((confirmed) => {
+    if (confirmed) {
+      let productId = $(this).data("id");
+      deleteProduct(productId);
+    }
+  });
 });
+
+///////////////////////////////////////////////////////////////////////////////////
+
+function checkDelete() {
+  return Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
+      }).then(() => {
+        location.reload();
+      });
+      // console.log("response");
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
